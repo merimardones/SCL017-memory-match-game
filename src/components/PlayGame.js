cdfd57367b9b2e374a28c50db14f27b821b9a3e4
Katherine-valcarce <katherine.v@live.cl>
2021-06-24 09:58:55 -0400

    orden llaves

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -2,7 +2,6 @@ import pokemonCards from '../data/pokemonCards/pokemonCards.js';
 
 //Array de cartas Pokémon
  const cards = pokemonCards.items; 
-
  //Array de cartas duplicadas
  const cardsPair = cards.concat(cards); 
 // función shuffle para barajar las cartas
@@ -18,52 +17,54 @@ import pokemonCards from '../data/pokemonCards/pokemonCards.js';
 
 const play = () => {
  
-//-----Creación de HTML dinámico
- const containerPlay = document.createElement('div');
- containerPlay.className = 'containerPlay'; // container padre del juego
-  
- const containerGrid = document.createElement('div');
- containerGrid.className = 'containerGrid'; 
- containerPlay.appendChild(containerGrid);// container donde va el grid de cartas
- 
- //----Creación de divs para cada reverso de carta (se inserta imagen en los 18 divs)
- const gridBoard = function(cardsPair) {
+  //-----Creación de HTML dinámico
+  const containerPlay = document.createElement('div');
+  containerPlay.className = 'containerPlay'; // container padre del juego
     
-  containerGrid.innerHTML = "";
-    for (let i = 0; i < cardsPair.length; i++) { //recorre la data
+  const containerGrid = document.createElement('div');
+  containerGrid.className = 'containerGrid'; 
+  containerPlay.appendChild(containerGrid);// container donde va el grid de cartas
+  
+  //----Creación de divs para cada reverso de carta (se inserta imagen en los 18 divs)
+  const gridBoard = function(cardsPair) {
+      
+    containerGrid.innerHTML = "";
+      for (let i = 0; i < cardsPair.length; i++) { //recorre la data
 
-    let cardShown = document.createElement("div");
-    cardShown.className = "cardShown";
-    let cardBack = document.createElement('img');
-    cardBack.className = "cardBack";
-    cardBack.setAttribute("src", "./components/reverso_carta.png"); // se crea un div para cada carta
-    cardShown.appendChild(cardBack);
+        let cardShown = document.createElement("div");
+        cardShown.className = "cardShown";
+        let cardBack = document.createElement('img');
+        cardBack.className = "cardBack";
+        cardBack.setAttribute("src", "./components/reverso_carta.png"); // se crea un div para cada carta
+        cardShown.appendChild(cardBack);
+        
+        
+        cardBack.addEventListener("click", () => {
+          cardBack.setAttribute("src", cardsPair[i].image);
+          cardBack.id= (cardsPair[i].id);
+          cardBack.className = "frontCard";
+         })
+         
+           /*if(frontCard.length>2)*/
     
+        /*let shuffleCards = shuffle(cardsPair);
+        let matchedCards= []; 
+        let frontCard = []; 
     
-    cardBack.addEventListener("click", () => {
-      cardBack.setAttribute("src", cardsPair[i].image);
-      cardBack.id= (cardsPair[i].id);
-      })
-      console.log(cardBack);
-      gridBoard(cardsPair); 
-  
-  /*let shuffleCards = shuffle(cardsPair);
-  let matchedCards= []; 
-  let pickedCards = []; 
-  
-      const flipCard = (target) => {
-        if (pickedCards.length>2 && !pickedCards.includes(target) && !matchedCards.includes(target)){
-          pickedCards.push(target); 
-          target.classList.toggle('flipped');
-          if(pickedCards.length === 2) {
-        setTimeout(matchedCard, 600); */
+        const flipCard = (target) => {
+          if (frontCard.length>2 && !frontCard.includes(target) && !matchedCards.includes(target)){
+            frontCard.push(target); 
+            target.classList.toggle('flipped');
+            if(frontCard.length === 2) {
+          setTimeout(matchedCard, 600);*/
 
+        containerGrid.appendChild(cardShown);
       }   
 
+  }
+  gridBoard(cardsPair); 
 
-  containerGrid.appendChild(cardShown);
- }
- return containerPlay;
-  };
+  return containerPlay;
+};
 
 export default play
