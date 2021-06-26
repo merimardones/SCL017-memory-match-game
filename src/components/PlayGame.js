3b4004151d3a294fa9ff9b62eeb23905973c4499
merimardones <mgmardones@uc.cl>
2021-06-26 10:10:12 -0400

    funcionando, código limpio y media querys segunda pantalla

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -6,7 +6,7 @@ import pokemonCards from '../data/pokemonCards/pokemonCards.js';
  const cardsPair = cards.concat(cards); 
 
 
-// función shuffle para barajar las cartas
+//Función shuffle para barajar las cartas
   function shuffle (array){
      for(let i= array.length -1; i > 0; i--) {
          let j = Math.floor(Math.random() * (i+1));
@@ -15,18 +15,16 @@ import pokemonCards from '../data/pokemonCards/pokemonCards.js';
          array [j] = temporary;
      }
  }
-  shuffle(cardsPair); // función shuffle para barajar las cartas
-  
-  const chosenClick = [];
-  let primeraCartagirada = true;
-   
+  shuffle(cardsPair); 
 
   
+//Constante para guardar cartas clickeadas
+  const chosenClick = []; 
 
 
 const play = () => {
     
-  //-----Creación de HTML dinámico
+  //Creación de HTML dinámico
   const containerPlay = document.createElement('div');
   containerPlay.className = 'containerPlay'; // container padre del juego
     
@@ -34,189 +32,54 @@ const play = () => {
   containerGrid.className = 'containerGrid'; 
   containerPlay.appendChild(containerGrid);// container donde va el grid de cartas
   
-  //----Creación de divs para cada reverso de carta (se inserta imagen en los 18 divs)
+  //Creación de divs para cada reverso de carta (se inserta imagen en los 18 divs)
   const gridBoard = function() {
-
       
     containerGrid.innerHTML = "";
-      for (let i = 0; i < cardsPair.length; i++) { //recorre la data
+      for (let i = 0; i < cardsPair.length; i++) { //Recorre la data
 
         let cardShown = document.createElement("div");
         cardShown.className = "cardShown";
-        let cardBack = document.createElement('img');
+        let cardBack = document.createElement('img'); 
         cardBack.className = "cardBack";
-        cardBack.setAttribute("src", "./components/reverso_carta.png"); // se crea un div para cada carta
-        cardShown.appendChild(cardBack);
-        
-        
-        cardBack.addEventListener("click", function () {
-          if (chosenClick.length < 2) {         
-          cardBack.setAttribute("src", cardsPair[i].image); //dar vuelta carta boca arriba (con el pokémon visible).
-          /*let id = (cardsPair[i].id); */
-          cardBack.className = "frontCard";
-          chosenClick.push(cardsPair[i]);
-          /*console.log(id); */
-         
-          setTimeout (() => {
-           flipCardBack (cardsPair[i], cardBack); 
-          }, 1000);
-        }
-
-        });
-          
-
-          /*if(cardsPair[i]).matched) {
-          cardBack.setAttribute("src", cardsPair[i].image);
-        } */
-        
-        containerGrid.appendChild(cardShown);
+        cardBack.setAttribute("src", "./components/reverso_carta.png"); // Se inserta la imagen del reverso 
+        cardShown.appendChild(cardBack);    
+        cardBack.addEventListener("click", function () {      //Función al clickear una carta
+          if (chosenClick.length < 2) {                       // Permite clickear solo dos cartas
+            cardBack.setAttribute("src", cardsPair[i].image); //Mostrar el pokémon cuando se gira. 
+            chosenClick.push(cardsPair[i]);                   //Guarda las cartas clickeadas en línea 22       
+            setTimeout (() => {                               // Tiempo para el girar las cartas
+              flipCardBack (cardsPair[i], cardBack); 
+            }, 1000);
+          }
+        });                
+        containerGrid.appendChild(cardShown);                 // Creación del div para cada carta
       }  
-    }
+  }
     gridBoard(); 
 
+    // Función que une el girar con el match
     function flipCardBack(cardData, imageCard){
       imageCard.setAttribute("src", cardData.image); 
-     /* if (cardData.matched) {
-        return false;
-      } 
-      */
-     // chosenClick.push(cardData);
-      
-      setTimeout(function() {
-        finalMatch (chosenClick);
-      }, 1000);
+          setTimeout(function() {
+            finalMatch (chosenClick);
+          }, 1000);
     }
 
-
-      function finalMatch (arrayOfChosenClick) {
-        if (arrayOfChosenClick.length == 2) {
+    
+    function finalMatch (arrayOfChosenClick) {
+      if (arrayOfChosenClick.length == 2) {
           if(arrayOfChosenClick[0].id == arrayOfChosenClick[1].id) {
             arrayOfChosenClick [0].matched = true;
             arrayOfChosenClick [1].matched = true;
             alert("finalmatch");
-
           }
           else (gridBoard())
-          arrayOfChosenClick.length =0;
-          
-          //cardBack.setAttribute("src", arrayOfChosenClick[0].image); 
-          
-        }
+          arrayOfChosenClick.length =0;                       
       }
-           
+    }    
    return containerPlay;
 };
 
 export default play
 
-/*function voltearCartas(){
-  if (arrayOfChosenClick.length <2 ){
-    imageCard.setAttribute("src", cardData.image);
-  }
-    if (arrayOfChosenClick.length ==0){ 
-      arrayOfChosenClick[0].id
-    }
-  
-}*/
-
-/* import pokemonCards from '../data/pokemonCards/pokemonCards.js';
-
-//Array de cartas Pokémon
- const cards = pokemonCards.items; 
- //Array de cartas duplicadas
- const cardsPair = cards.concat(cards); 
-
-
-// función shuffle para barajar las cartas
-  function shuffle (array){
-     for(let i= array.length -1; i > 0; i--) {
-         let j = Math.floor(Math.random() * (i+1));
-         let temporary = array[i];
-         array [i] = array [j];
-         array [j] = temporary;
-     }
- }
-  shuffle(cardsPair); // función shuffle para barajar las cartas
-  
-  const chosenClick = [];
-
-const play = () => {
-    
-  //-----Creación de HTML dinámico
-  const containerPlay = document.createElement('div');
-  containerPlay.className = 'containerPlay'; // container padre del juego
-    
-  const containerGrid = document.createElement('div');
-  containerGrid.className = 'containerGrid'; 
-  containerPlay.appendChild(containerGrid);// container donde va el grid de cartas
-  
-  //----Creación de divs para cada reverso de carta (se inserta imagen en los 18 divs)
-  const gridBoard = function() {
-      
-    containerGrid.innerHTML = "";
-      for (let i = 0; i < cardsPair.length; i++) { //recorre la data
-
-        let cardShown = document.createElement("div");
-        cardShown.className = "cardShown";
-        let cardBack = document.createElement('img');
-        cardBack.className = "cardBack";
-        cardBack.setAttribute("src", "./components/reverso_carta.png"); // se crea un div para cada carta
-        cardShown.appendChild(cardBack);
-        
-        
-        cardBack.addEventListener("click", function () {
-          cardBack.setAttribute("src", cardsPair[i].image); //dar vuelta carta boca arriba (con el pokémon visible).
-          //let id = (cardsPair[i].id); 
-          cardBack.className = "frontCard";
-          //console.log(id); 
-
-          
-          setTimeout (() => {
-           flipCardBack (cardsPair[i], cardBack); 
-          }, 1000);
-
-        });
-          //if(cardsPair[i]).matched) {
-         // cardBack.setAttribute("src", cardsPair[i].image);
-        //} 
-        
-        containerGrid.appendChild(cardShown);
-      }  
- 
-    }
-    gridBoard(); 
-
-    function flipCardBack(cardData, imageCard){
-     // imageCard.setAttribute("src", cardData.image); 
-      if (cardData.matched) {
-        return false;
-      } 
-      
-      chosenClick.push(cardData);
-      
-      setTimeout(function() {
-        finalMatch (chosenClick);
-      }, 1000);
-
-
-       
-      function finalMatch (arrayOfChosenClick) {
-        if (arrayOfChosenClick.length == 2) {
-          if(arrayOfChosenClick[0].id === arrayOfChosenClick[1].id) {
-            arrayOfChosenClick [0].matched = true;
-            arrayOfChosenClick [1].matched = true;
-            alert("finalmatch");
-            //arrayOfChosenClick.length =0;
-          } 
-          arrayOfChosenClick.length =0;
-
-        
-          gridBoard(); 
-        }
-      }
-     }
-           
-   return containerPlay;
-};
-
-export default play */
