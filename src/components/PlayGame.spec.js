0425a40858f411e9ff6f5fadd2b0e42f0f49f970
merimardones <mgmardones@uc.cl>
2021-06-30 12:42:50 -0400

    test función shuffle

--- src/components/PlayGame.spec.js
+++ src/components/PlayGame.spec.js
@@ -3,8 +3,10 @@ import {shuffle} from './PlayGame.js';
 
 describe('Shuffle', () => {
     it('should shuffle the cards', () => {
-        console.log(shuffle);
+        let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                expect(shuffle(cards) == cards).toBe(false);
+            });
+        });
       
-    });
-  });
+   
   
